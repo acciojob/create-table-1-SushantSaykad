593a13575867a4c9262889d9b61d3e9f3a7ea469
Sushant Saykad <69181604+SushantSaykad@users.noreply.github.com>
2026-05-05 11:39:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function insert_Row() {
     //Write your code here
     const table = document.getElementById("sampleTable");
-    const newRow = table.insertRow(0);
+    const newRow = table.insertRow(0);// inserting 0 for top row
     const cell1 = newRow.insertCell(0);
     const cell2 = newRow.insertCell(1);
     cell1.innerHTML = "New Cell1";
